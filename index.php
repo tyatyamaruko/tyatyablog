2c6c87c9e9477fd0cb94e134b696259c57881c0a
ryotatanks9981 <67944448+ryotatanks9981@users.noreply.github.com>
2021-10-18 22:12:25 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/common.css">
+    <link rel="stylesheet" href="./css/index.css">
+    <title>ちゃちゃブログ</title>
+</head>
+<body>
+    <header>
+        <h1>ちゃちゃブログ</h1>
+    </header>
+    <main>
+        <nav>
+            <ul class="menu">
+                <li><a href="./tech.php">技術記事</a></li>
+                <li><a href="./tech.php">日常記事</a></li>
+                <li><a href="./tech.php">問い合わせ</a></li>
+            </ul>
+
+            <ul class="topic">
+                <li>
+                    <p class="title">最新記事</p>
+                    <p class="date">2021-10-19</p>
+                </li>
+                <li>
+                    <p class="title">最新記事</p>
+                    <p class="date">2021-10-19</p>
+                </li>
+                <li>
+                    <p class="title">最新記事</p>
+                    <p class="date">2021-10-19</p>
+                </li>
+                <li>
+                    <p class="title">最新記事</p>
+                    <p class="date">2021-10-19</p>
+                </li>
+                <li>
+                    <p class="title">最新記事</p>
+                    <p class="date">2021-10-19</p>
+                </li>
+            </ul>
+        </nav>
+
+        <div class="container">
+            <article class="top">
+                <h2>Hello World</h2>
+
+                <article class="description">
+                    <p>
+                        本サイトは主に技術ブログとして更新していく予定ではありますが、筆者の気分によってかなりの確率で愛犬についての投稿になる可能性があります。<br>
+                        また、技術ブログはWEB系(<span>HTML</span>, <span>CSS</span>, <span>JavaScript</span>, <span>PHP</span>)と
+                        ネイティブアプリ(特に<span>Swift</span>)やその周辺についての投稿が主になりますが、
+                        興味を持ったことについての投稿もたまにする予定です。<br>
+                        おもしろおかしく記事を書いて行けたらいいなと思っておりますので是非クスッと笑いながら読んでいただけますと光栄です。
+                    </p>
+                </article>
+            </article>
+        </div>
+
+        <div class="adsense"></div>
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
---
+++ tech.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/common.css">
+    <link rel="stylesheet" href="./css/tech.css">
+    <title>ちゃちゃブログ｜技術</title>
+</head>
+
+<body>
+    <header>
+        <h1>ちゃちゃブログ</h1>
+    </header>
+    <main>
+        <nav>
+            <ul class="menu">
+                <li><a href="./tech.php">技術記事</a></li>
+                <li><a href="./tech.php">日常記事</a></li>
+                <li><a href="./tech.php">問い合わせ</a></li>
+            </ul>
+
+            <ul class="topic">
+                <li>
+                    <p class="title">最新記事</p>
+                    <p class="date">2021-10-19</p>
+                </li>
+                <li>
+                    <p class="title">最新記事</p>
+                    <p class="date">2021-10-19</p>
+                </li>
+                <li>
+                    <p class="title">最新記事</p>
+                    <p class="date">2021-10-19</p>
+                </li>
+                <li>
+                    <p class="title">最新記事</p>
+                    <p class="date">2021-10-19</p>
+                </li>
+                <li>
+                    <p class="title">最新記事</p>
+                    <p class="date">2021-10-19</p>
+                </li>
+            </ul>
+        </nav>
+
+        <div class="container">
+            <article>
+                <h2>ジャンル</h2>
+
+                <ul>
+                    <li class="html">
+                        <div>
+                            <img src="./imgs/html-logo.png" alt="">
+                            <p>HTML</p>
+                        </div>
+                    </li>
+                    <li class="css">
+                        <div>
+                            <img src="./imgs/css-logo.png" alt="">
+                            <p>CSS</p>
+                        </div>
+                    </li>
+                    <li class="js">
+                        <div>
+                            <img src="./imgs/js-logo.png" alt="">
+                            <p>JavaScript</p>
+                        </div>
+                    </li>
+                    <li class="php">
+                        <div>
+                            <img src="./imgs/php-logo.png" alt="">
+                            <p>PHP</p>
+                        </div>
+                    </li>
+                    <li class="swift">
+                        <div>
+                            <img src="./imgs/swift-logo.jpeg" alt="">
+                            <p>Swift</p>
+                        </div>
+                    </li>
+                    <li class="ios">
+                        <div>
+                            <img src="./imgs/ios-logo.png" alt="">
+                            <p>iosアプリ開発</p>
+                        </div>
+                    </li>
+                    <li class="web">
+                        <div>
+                            <img src="./imgs/web-logo.png" alt="">
+                            <p>Webアプリ開発</p>
+                        </div>
+                    </li>
+                    <li class="other">
+                        <div>
+                            <img src="./imgs/pro-logo.jpeg" alt="">
+                            <p>その他</p>
+                        </div>
+                    </li>
+                </ul>
+            </article>
+        </div>
+
+        <div class="adsense"></div>
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
