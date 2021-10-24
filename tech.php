e4faab848307e6ae0162b3f82527fa4725204a35
ryotatanks9981 <67944448+ryotatanks9981@users.noreply.github.com>
2021-10-24 18:12:04 +0900

    tech.phpを編集

--- tech.php
+++ tech.php
@@ -1,3 +1,35 @@
+<?php
+require_once "./models/tech-article.php";
+require_once "./admin/env.php";
+
+try {
+    $pdo = new PDO(DSN, USERNAME, PASSWORD);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $sql = "select * from techarticles where 1 order by created_at desc limit 5";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute($data);
+
+    $articles = [];
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $articles[] = new TechArticle($row);
+    }
+
+    // var_dump($articles);/
+} catch (PDOException $e) {
+    // エラーが発生した場合は「500 Internal Server Error」でテキストとして表示して終了する
+    // - もし手抜きしたくない場合は普通にHTMLの表示を継続する
+    // - ここではエラー内容を表示しているが， 実際の商用環境ではログファイルに記録して， Webブラウザには出さないほうが望ましい
+    header('Content-Type: text/plain; charset=UTF-8', true, 500);
+    exit($e->getMessage());
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -16,34 +48,25 @@
         <h1>ちゃちゃブログ</h1>
     </header>
     <main>
-        <nav>
+    <nav>
             <ul class="menu">
                 <li><a href="./tech.php">技術記事</a></li>
                 <li><a href="./tech.php">日常記事</a></li>
-                <li><a href="./tech.php">問い合わせ</a></li>
+                <!-- <li><a href="./tech.php">問い合わせ</a></li> -->
             </ul>
 
             <ul class="topic">
                 <li>
-                    <p class="title">最新記事</p>
-                    <p class="date">2021-10-19</p>
+                    <h5>最新記事</h5>
                 </li>
-                <li>
-                    <p class="title">最新記事</p>
-                    <p class="date">2021-10-19</p>
-                </li>
-                <li>
-                    <p class="title">最新記事</p>
-                    <p class="date">2021-10-19</p>
-                </li>
-                <li>
-                    <p class="title">最新記事</p>
-                    <p class="date">2021-10-19</p>
-                </li>
-                <li>
-                    <p class="title">最新記事</p>
-                    <p class="date">2021-10-19</p>
-                </li>
+                <?php foreach ($articles as $article) : ?>
+                    <li>
+                        <a href="./detail.php?id=<?= $article->id ?>">
+                            <p class="title"><?= $article->title ?></p>
+                            <p class="date"><?= $article->created_at ?></p>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
             </ul>
         </nav>
 
@@ -54,50 +77,64 @@
                 <ul>
                     <li class="html">
                         <div>
-                            <img src="./imgs/html-logo.png" alt="">
-                            <p>HTML</p>
+                            <a href="./list.php?lang=html">
+                                <img src="./imgs/html-logo.png" alt="">
+                                <p>HTML</p>
+                            </a>
                         </div>
                     </li>
                     <li class="css">
                         <div>
-                            <img src="./imgs/css-logo.png" alt="">
-                            <p>CSS</p>
+                            <a href="./list.php?lang=css">
+                                <img src="./imgs/css-logo.png" alt="">
+                                <p>CSS</p>
+                            </a>
                         </div>
                     </li>
                     <li class="js">
                         <div>
-                            <img src="./imgs/js-logo.png" alt="">
-                            <p>JavaScript</p>
+                            <a href="./list.php?lang=javascript">
+                                <img src="./imgs/js-logo.png" alt="">
+                                <p>JavaScript</p>
+                            </a>
                         </div>
                     </li>
                     <li class="php">
                         <div>
-                            <img src="./imgs/php-logo.png" alt="">
-                            <p>PHP</p>
+                            <a href="./list.php?lang=php">
+                                <img src="./imgs/php-logo.png" alt="">
+                                <p>PHP</p>
                         </div>
                     </li>
                     <li class="swift">
                         <div>
-                            <img src="./imgs/swift-logo.jpeg" alt="">
-                            <p>Swift</p>
+                            <a href="./list.php?lang=swift">
+                                <img src="./imgs/swift-logo.jpeg" alt="">
+                                <p>Swift</p>
                         </div>
                     </li>
                     <li class="ios">
                         <div>
-                            <img src="./imgs/ios-logo.png" alt="">
-                            <p>iosアプリ開発</p>
+                            <a href="./list.php?lang=ios">
+                                <img src="./imgs/ios-logo.png" alt="">
+                                <p>iosアプリ開発</p>
+                            </a>
                         </div>
                     </li>
                     <li class="web">
                         <div>
-                            <img src="./imgs/web-logo.png" alt="">
-                            <p>Webアプリ開発</p>
+                            <a href="./list.php?lang=web">
+                                <img src="./imgs/web-logo.png" alt="">
+                                <p>Webアプリ開発</p>
+                            </a>
                         </div>
                     </li>
                     <li class="other">
                         <div>
-                            <img src="./imgs/pro-logo.jpeg" alt="">
-                            <p>その他</p>
+                            <a href="./list.php?lang=other">
+                                <img src="./imgs/pro-logo.jpeg" alt="">
+                                <p>その他</p>
+                            </a>
                         </div>
                     </li>
                 </ul>
