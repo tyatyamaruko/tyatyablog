bdb9ea6bdcde7fbc22ad5a35848c2a950a943571
ryotatanks9981 <67944448+ryotatanks9981@users.noreply.github.com>
2021-11-12 23:38:52 +0900

    favicon set

--- assets/hd.php
+++ assets/hd.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/87419a29af.js" crossorigin="anonymous"></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6920571237174813" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./js/prism.css">
     <script src="./js/prism.js"></script>
+    <link rel="icon" href="./imgs/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="./imgs/apple-touch-icon-180x180.png">
     <link rel="stylesheet" href="./css/style.css">
     <title>ちゃちゃブログ</title>
 </head>
